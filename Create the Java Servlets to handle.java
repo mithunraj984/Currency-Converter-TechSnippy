bf616a10fc80c678358a9b2e0068b183d8edfd0c
mithun raj <109740856+mithunraj984@users.noreply.github.com>
2024-01-16 21:48:13 +0530

    Add files via upload

---
+++ Create the Java Servlets to handle.java
@@ -0,0 +1,13 @@
+/* Create the Java Servlets to handle the HTTP requests and responses. The servlets should communicate with the applet to get the latest exchange rates and perform the currency conversion */
+
+
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class CurrencyConverterServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Code to handle the HTTP request and response
+    }
+}
